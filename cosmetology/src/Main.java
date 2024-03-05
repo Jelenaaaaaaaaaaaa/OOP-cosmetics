8e90b840b1b3aa3be9ea220c9f157a9c48221ec3
Jelenaaaaaaaaaaaa <162324837+Jelenaaaaaaaaaaaa@users.noreply.github.com>
2024-03-05 18:31:20 +0200

    Update Main.java

--- cosmetology/src/Main.java
+++ cosmetology/src/Main.java
@@ -7,24 +7,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hii , welcome to our cosmetology!");
-//        System.out.println("Choose which procedure you want and then you will see total cost, options and time");
-//        System.out.println("Procedures info ( c1-c3) - skincare(s1-s2), correction(cr1-cr2), massage(m1-m2)");
-        System.out.println("Have you ever been in out cosmetology? true / false");
-        boolean yesno = sc.hasNextBoolean();
-        if (yesno) {
+        System.out.println("We have skincare , correction, massage procedures and also catalogue with price list , time and procedures name");
+        System.out.println("ANYWAYS");
+        System.out.println("Have you ever been in our cosmetology? true- yes / false - no");
+        boolean y= sc.nextBoolean();
+        if (y) {
             System.out.println("WOW you already have an expirience, let's start !!!!");
-        } else {
-            System.out.println("Now we asking you for a couple of questions ");
-            System.out.println("Do you care about your skin ? ");
+            System.out.println("We hope you will enjoy here ^^");
+            System.out.println("We try do all procedures perfectly ");
         }
-//            boolean yesno2 = sc.hasNextBoolean();
-//            if (yesno2) {
-//                System.out.println("Fine ,  let's go to the last question!!");
-//            } else {
-//                System.out.println("WAIT!!! HOW ?? WE NEED TO DO ALL BEAUTY PROCEDURES, anyway..let's go to the last question");
-
+        else {
+            System.out.println("Now we asking you for a question ");
+            System.out.println("Do you care about your skin quality, health , face parameters? true-yes/ false-no ");
+            boolean n = sc.nextBoolean();
+            if (n) {
+                System.out.println("Fine ,  let's go to the fantastic procedures!!");
                 System.out.println("We hope you will enjoy here ^^");
                 System.out.println("We try do all procedures perfectly ");
+            } else {
+                System.out.println("WAIT!!! HOW ?? WE NEED TO DO ALL BEAUTY PROCEDURES, anyway..let's go to correct it");
+                System.out.println("We hope you will enjoy here ^^");
+                System.out.println("We try do all procedures perfectly ");
+            }
+        }
+        System.out.println("*AFTER PROCEDURES*");
+        System.out.println( " Do you enjoy in our cosmetology? true-yes / false -no");
+        boolean c = sc.nextBoolean();
+        if (c){
+            System.out.println(" We are so happy to hear that, see you soon ❤️");
+        }else {
+            System.out.println( " We are so sorry, that we did something wrong, that you didn't like....");
+            System.out.println("\nThis is discount coupon, maybe you will change you opinion about us!");
+        }
             
 
 
